0f6feb32dd4c4b9279e969dbd55231db47f53258
edpoole <ed@radweb.co.uk>
2014-09-03 18:46:28 +0100

    tweaking animations for menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,12 @@
 
+var toggle = false;
 $('.navigation__toggle').click(function(e) {
 	e.preventDefault();
 	$(this).toggleClass('is-open');
-	$('.lead--home').toggleClass('is-blurred');
-	$('.navigation__model').toggleClass('is-open');
+	if (toggle) {
+		$('.navigation__link').removeClass('is-open').addClass('is-closed');
+	} else {
+		$('.navigation__link').removeClass('is-closed').addClass('is-open');	
+	}
+	toggle = !toggle;
 });
