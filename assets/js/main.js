f6b4a1c5383419163a12803e0c8378d6b20e19b1
edpoole <ed@radweb.co.uk>
2014-09-03 17:58:07 +0100

    Animation on navigation hover

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,6 @@
 $('.navigation__toggle').click(function(e) {
 	e.preventDefault();
 	$(this).toggleClass('is-open');
+	$('.lead--home').toggleClass('is-blurred');
+	$('.navigation__model').toggleClass('is-open');
 });
